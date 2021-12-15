06bedb1f5e5288538a3c237fdc51278bd6d96641
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-12-15 09:53:41 +0530

    :passport_control: chore(Get JWT token & Get forgot password token)
    - Method to get jwt token using jsonwebtoken
    - Method to create hashed forgot password token and save it to database
      and return only the string (non-hashed) token to user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,8 +2,9 @@ import {Schema, model, Document} from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
-export interface IUser extends Document {
+export interface IUser {
 	firstName: string;
 	lastName: string;
 	email: string;
@@ -14,12 +15,18 @@ export interface IUser extends Document {
 	};
 	role: string;
 	forgotPasswordToken: string;
-	forgotPasswordExpiry: Date;
+	forgotPasswordExpiry: number;
 	createdAt: Date;
 	updatedAt: Date;
 }
 
-const UserSchema = new Schema<IUser>(
+export interface IUserDocument extends IUser, Document {
+	comparePassword: (userPassword: string) => Promise<boolean>;
+	getJwtToken: () => string;
+	getForgotPasswordToken: () => string;
+}
+
+const UserSchema = new Schema<IUserDocument>(
 	{
 		firstName: {
 			type: String,
@@ -66,8 +73,8 @@ const UserSchema = new Schema<IUser>(
 	}
 );
 
-// encrypt password before save
-UserSchema.pre<IUser>('save', async function (next) {
+// encrypt password before save - HOOK
+UserSchema.pre<IUserDocument>('save', async function (next) {
 	/**only runs when the user will be created and when the user wants to change the password
 	 * https://mongoosejs.com/docs/api.html#document_Document-isModified
 	 *
@@ -77,6 +84,40 @@ UserSchema.pre<IUser>('save', async function (next) {
 	this.password = await bcrypt.hash(this.password, 10);
 });
 
-const UserModel = model<IUser>('User', UserSchema);
+// compare the password with passed on user password
+UserSchema.methods.comparePassword = async function (userPassword: string): Promise<boolean> {
+	return await bcrypt.compare(userPassword, this.password);
+};
+
+// create and return jwt token
+UserSchema.methods.getJwtToken = function (): string {
+	const jwt_secret = `${process.env.JWT_SECRET}`;
+	const jwt_expiry = `${process.env.JWT_EXPIRY}`;
+
+	return jwt.sign({id: this._id}, jwt_secret, {
+		expiresIn: jwt_expiry
+	});
+};
+
+// generate forgot password token
+UserSchema.methods.getForgotPasswordToken = function (): string {
+	// generate a long and random string
+	const forgotToken = crypto.randomBytes(20).toString('hex');
+
+	// getting a hash - make sure to get a hash on the backend
+	this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest('hex');
+
+	// time of token
+	this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
+
+	/**
+	 * return the forgotPass string, but in database, the encrypted value i.e forgotPasswordToken will be saved. When user sends back the forgotToken string back then it will be again encrypted and compared with the earlier encrypted value saved in db.
+	 * If the both value matches that means the value was not tempered, the value
+	 * was not being manipulated and you're getting exactly same.
+	 */
+	return forgotToken;
+};
+
+const UserModel = model<IUserDocument>('User', UserSchema);
 
 export default UserModel;
