7ba89688e163c03e46442388ab6c55c1c358cfbf
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2022-09-25 18:08:03 +0530

    chore: :construction: Regular modifications

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { Provider } from "react-redux";
+import store from "../store";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
