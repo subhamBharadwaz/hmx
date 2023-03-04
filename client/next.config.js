bef70692ea69d1d0162892dc982be053bfeecd25
Restyled.io <commits@restyled.io>
2023-03-04 16:36:40 +0000

    Restyled by clang-format

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  images: {
-    domains: ["res.cloudinary.com"],
+  reactStrictMode : false,
+  swcMinify : true,
+  images : {
+    domains : [ "res.cloudinary.com" ],
   },
 };
 
