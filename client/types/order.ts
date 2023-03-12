dab8a1e94f8732c194d0b6f56af5ecd7ccab445b
Subham Shyamal Bharadwaz <subhamsbharadwaz@gmail.com>
2023-03-12 22:13:12 +0530

    feat: :label: Order Types

--- client/types/order.ts
+++ client/types/order.ts
@@ -34,6 +34,6 @@ export interface IOrder {
   taxAmount: number;
   shippingAmount: number;
   totalAmount: number;
+  orderStatus?: OrderStatusType;
   createdAt?: Date;
-  orderStatus: OrderStatusType;
 }
