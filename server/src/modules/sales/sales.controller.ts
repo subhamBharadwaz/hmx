e0c6c6199b1dd27d6a20f9a80443e84e0bb1fb79
Subham Shyamal Bharadwaz <subhamsbharadwaz@gmail.com>
2023-03-13 14:17:21 +0530

    feat: :sparkles: Get the total revenue of each state and also can query by year/month

--- server/src/modules/sales/sales.controller.ts
+++ server/src/modules/sales/sales.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /* eslint-disable import/prefer-default-export */
 import {Request, Response, NextFunction} from 'express';
 
@@ -5,11 +6,11 @@ import {BigPromise} from '../../middlewares';
 
 import {HttpStatusCode} from '../../types/http.model';
 import {APIError} from '../../utils';
-import {getSales} from './sales.service';
+import {getSales, getSalesByState} from './sales.service';
 
 /** 
 @desc    Get Sales Details
-@route   GET /api/v1/order/id
+@route   GET /api/v1/sales/:year/:month
 @access  Private
 */
 export const getSalesDataHandler = BigPromise(
@@ -26,3 +27,27 @@ export const getSalesDataHandler = BigPromise(
 		res.status(200).json({success: true, salesData});
 	}
 );
+
+/** 
+@desc    Get Sales Details by States
+@route   GET /api/v1/sales/state
+@access  Private
+*/
+export const getSalesByStates = BigPromise(async (req: Request, res: Response) => {
+	const {year, month} = req.query;
+
+	const matchQuery: any = {'shippingInfo.state': {$exists: true}};
+
+	if (year && month) {
+		matchQuery.$expr = {
+			$and: [
+				{$eq: [{$year: '$createdAt'}, parseInt(year as string)]},
+				{$eq: [{$month: '$createdAt'}, parseInt(month as string)]}
+			]
+		};
+	}
+
+	const salesData = await getSalesByState(matchQuery);
+
+	res.status(200).json({success: true, data: salesData});
+});
--- server/src/modules/sales/sales.route.ts
+++ server/src/modules/sales/sales.route.ts
@@ -4,10 +4,11 @@ import {Router} from 'express';
 import {isLoggedIn, customRole} from '../../middlewares';
 
 // import controllers
-import {getSalesDataHandler} from './sales.controller';
+import {getSalesDataHandler, getSalesByStates} from './sales.controller';
 
 const router = Router();
 
 router.route('/sales/:year/:month').get(isLoggedIn, customRole('admin'), getSalesDataHandler);
+router.route('/sales/state').get(isLoggedIn, customRole('admin'), getSalesByStates);
 
 export default router;
