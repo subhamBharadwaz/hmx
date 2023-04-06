853fa60185b0f9de4d096b91503bb7034c917f82
Subham Shyamal Bharadwaz <72348711+subhamBharadwaz@users.noreply.github.com>
2023-04-06 17:48:32 +0530

    fix: :bug: test

--- server/src/utils/cookieToken.ts
+++ server/src/utils/cookieToken.ts
@@ -7,7 +7,8 @@ const cookieToken = (user: IUserDocument, res: Response) => {
 	const options = {
 		expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
 		httpOnly: true,
-		secure: true
+		secure: true,
+		SameSite: 'none'
 	};
 
 	res.status(200).cookie('token', token, options).json({
